914e3462e8df531d908d9ca74904da175bb384c0
Blankanator <7fuzi.blanka@gmail.com>
2024-02-22 15:05:28 +0100

    first commit

--- Pascal/Form1.cs
+++ Pascal/Form1.cs
@@ -23,6 +23,7 @@ namespace Pascal
 
                     int x = Faktorialis(sor) / (Faktorialis(oszlop) * Faktorialis(sor - oszlop));
                     button.Text = x.ToString();
+                    //ez egy comment
                 }
             }
         }
